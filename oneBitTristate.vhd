200cd2538e94d77a430fd28dd799b36efdf118c5
Surya Vasudev <suryavasudev05@gmail.com>
2025-11-27 20:11:40 -0500

    fixed oneBitTristate.vhd

--- oneBitTristate.vhd
+++ oneBitTristate.vhd
@@ -2,7 +2,8 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 ENTITY oneBitTristate IS
-        enable  : IN  STD_LOGIC;
+    PORT(
+        enable : IN  STD_LOGIC;
         input   : IN  STD_LOGIC;
         output  : OUT STD_LOGIC
     );
